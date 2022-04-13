f8d951bb24a1da4a96403ac37287b0f03aeef898
Rigoki <Damien.ferrari.19@gmail.com>
2022-04-13 14:31:12 +0200

    Ajoutez des fichiers projet.

---
+++ MonChrono.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Chrono
+{
+    partial class MonChrono
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btntst = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btntst
+            // 
+            this.btntst.AutoSize = true;
+            this.btntst.BackColor = System.Drawing.Color.Crimson;
+            this.btntst.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btntst.Location = new System.Drawing.Point(104, 208);
+            this.btntst.Name = "btntst";
+            this.btntst.Size = new System.Drawing.Size(114, 47);
+            this.btntst.TabIndex = 0;
+            this.btntst.Text = "Start !";
+            this.btntst.UseVisualStyleBackColor = false;
+            this.btntst.UseWaitCursor = true;
+            this.btntst.Click += new System.EventHandler(this.btntst_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Gold;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(104, 36);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(10);
+            this.label1.Size = new System.Drawing.Size(297, 137);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "0 : 0";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.UseWaitCursor = true;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.BackColor = System.Drawing.Color.Crimson;
+            this.button1.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(287, 208);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 47);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Reset !";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.UseWaitCursor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MonForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 351);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btntst);
+            this.Name = "MonForm";
+            this.Text = "Form1";
+            this.UseWaitCursor = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btntst;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ MonChrono.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Chrono
+{
+    public partial class MonChrono : Form
+    {
+        private int sec = 0;
+        private int min = 0;
+        bool activation = false;
+        int nbclick = 0;
+        public MonChrono()
+        {
+            InitializeComponent();
+        }
+
+        private void btntst_Click(object sender, EventArgs e)
+        {
+            activation = !activation;
+            timer1.Enabled = activation;
+            if(activation)
+            {
+                btntst.Text = "Stop !";
+            } else
+            {
+                btntst.Text = "Start !";
+            }
+
+               
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            nbclick++;
+            label1.Text = "X _ X";
+            if(nbclick == 5)
+            {
+              
+                label1.Text = "è _ é";
+            }
+            if(nbclick== 7)
+            {
+                label1.Text = "STAPH!";
+                nbclick = 0;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           
+            sec++;
+            label1.Text = $"{min.ToString()} : {sec.ToString()}";
+            if(sec.ToString() == "59")
+            {
+                min++;
+                sec = 0;
+            }
+                
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "0 : 0";
+            sec = 0; 
+            min = 0;
+        }
+    }
+}
